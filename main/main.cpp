0e997a6c28b5db6768706fe7d4130e4355278a1c
Liu Yi <2509144806@qq.com>
2025-03-28 15:21:51 +0800

    update main.c

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,17 @@
 #include "../log/log.h"
-
+#include "../threadpool/threadpool.h"
+#include "../task/calculateTask.h"
 int main(){
-    LOG_INIT();
-    LOG_DEBUG("hello");
-    LOG_INFO("hello");
-    LOG_WARN("hello");
-    LOG_ERROR("hello");
-    LOG_FATAL("hello");
+    
+    // 创建线程池
+    ThreadPool pool(4);
+    // 提交任务
+    for (int i = 0; i < 10; i++)
+    {
+        Task *task = new calculateTask(i * 10, (i + 1) * 10 - 1);
+        pool.submit(task);
+    }
+    // 等待所有任务完成
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    return 0;
 }
