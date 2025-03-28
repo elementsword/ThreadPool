8ec4e67756cedcf6c6624f000bfab1e13b18559b
Liu Yi <2509144806@qq.com>
2025-03-28 15:18:47 +0800

    threadpool class finish

---
+++ threadpool/threadpool.cpp
@@ -0,0 +1,57 @@
+#include "threadpool.h"
+ThreadPool::ThreadPool(int size) : poolSize(size), isStop(false)
+{
+    for (size_t i = 0; i < poolSize; i++)
+    {
+        _threads.emplace_back(std::thread(&ThreadPool::workThread, this));
+    }
+}
+ThreadPool::~ThreadPool()
+{
+    std::unique_lock<std::mutex> lock(queueMutex);
+    isStop = true;
+    // 唤醒所有线程
+    condition.notify_all();
+    for (size_t i = 0; i < poolSize; i++)
+    {
+        // 等待资源释放退出
+        _threads[i].join();
+    }
+}
+// 提交任务到线程池
+void ThreadPool::submit(Task *task)
+{
+    std::unique_lock<std::mutex> lock(queueMutex);
+    // 如果线程池已经停止，抛出异常
+    if (isStop)
+    {
+        throw std::runtime_error("ThreadPool is stopped");
+    }
+    // 将任务添加到队列
+    _queue.push(task);
+    // 唤醒一个线程
+    condition.notify_one();
+}
+void ThreadPool::workThread()
+{
+    while (true)
+    {
+        Task *task = nullptr;
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            // 等待任务
+            condition.wait(lock, [this] { return !this->_queue.empty() || this->isStop; });
+            // 如果线程池已经停止，退出线程
+            if (isStop && _queue.empty())
+            {
+                return;
+            }
+            // 取出任务
+            task = _queue.front();
+            _queue.pop();
+        }
+        // 执行任务
+        task->execute();
+        delete task;
+    }
+}
--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -4,12 +4,15 @@
 #include <vector>
 #include <queue>
 #include <mutex>
+#include <condition_variable>
 #include "../task/task.h"
 class ThreadPool
 {
 public:
     ThreadPool(int);
     ~ThreadPool();
+    // 提交任务到线程池
+    void submit(Task* task);
 private:
     // 线程池大小
     int poolSize;
@@ -23,6 +26,10 @@ private:
     std::queue<Task*> _queue;
     //锁(保护队列)
     std::mutex queueMutex;
+    // 条件变量(用于线程同步)
+    std::condition_variable condition;
+    //工作逻辑
+    void workThread();
 };
 
 #endif
