ac3b20300ef524d12aecb22664ecb58aaf9e9026
Liu Yi <2509144806@qq.com>
2025-04-01 17:49:41 +0800

    add boardcastTask

---
+++ task/boardcastTask.cpp
@@ -0,0 +1,25 @@
+#include "boardcastTask.h"
+
+// 构造函数
+broadcastTask::broadcastTask(std::string message, int clientFd, std::vector<int> clients):
+    message(message), clientFd(clientFd), clients(clients)
+{
+
+}
+
+// 析构函数
+broadcastTask::~broadcastTask(){
+
+}
+
+void broadcastTask::execute()
+{
+    for(int i:clients)
+    {
+        // 发送消息给其他客户端
+        if(i != clientFd)
+        {
+            send(i, message.c_str(), message.size(), 0);
+        }
+    }
+}
