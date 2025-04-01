2e28f7764f4ed2db974aba6d0ed0e3e9703cf0ce
Liu Yi <2509144806@qq.com>
2025-04-01 17:49:41 +0800

    add boardcastTask

---
+++ task/boardcastTask.h
@@ -0,0 +1,22 @@
+#ifndef __BROADCASTTASK_H__
+#define __BROADCASTTASK_H__
+#include "task.h"
+#include <vector>
+#include <sys/socket.h>
+class broadcastTask : public Task
+{
+public:
+    // 构造函数
+    broadcastTask(std::string message,int clientFd,std::vector<int> clients);
+
+    // 析构函数
+    ~broadcastTask();
+
+    void execute();
+
+private:
+    std::string message; // 消息
+    int clientFd;       // 客户端套接字
+    std::vector<int> clients; // 客户端列表
+};
+#endif // __BROADCASTTASK_H__
